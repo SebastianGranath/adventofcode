bafdf7a0281a70c9de80292813a1bb0c6d76ee3d
Sebastian Granath <sebgr736@student.liu.se>
2023-12-07 20:54:44 +0100

    sucess day 1

--- day1.py
+++ day1.py
@@ -1,6 +1,24 @@
-import file
-file.open('day1input.py', 'r')
+with open('day1input.txt', 'r') as r:
+    data = r.readlines()
+print(data)
+line_nr = []
+for line in data:
+    number_log = []
+    for char in line:
+        try:
+            print(int(char))
+            number_log.append(int(char))
+        except:
+            print('Nan')
 
-print('test')
+    last = number_log.pop(-1)
+    if len(number_log) >= 1:
+        first = number_log.pop(0)
+    else:
+        first = last
 
-file.close()
+    line_nr.append(first*10+last)
+print(line_nr, sum(line_nr))
+
+
+
