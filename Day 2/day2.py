26750e8231f9ac715cca1ab55605eb19e1b157b7
Sebastian Granath <sebgr736@student.liu.se>
2023-12-08 17:21:06 +0100

    need to finish the loop te extract the results table

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,3 +1,28 @@
-with open('day2input.txt','r') as f:
+import numpy as np
+with open('day2input.txt', 'r') as f:
     data = f.readlines()
-print(data)
+
+
+colors = [' red', ' green', ' blue']
+lims = [6, 5, 4]
+for line in data:
+    line = line.split(':')
+    id = line[0].split(' ')[-1]
+    games = line[1].split(';')
+    results = np.zeros((3, len(games)))
+
+    for i in range(len(games)):
+        for j in range(len(colors)):
+            try:
+                # ERRORS IN THIS SECTION
+                results[j][i] = games[i][games[i].index(colors[j])-1]
+                print(results[j][i])
+            except:
+                results[j][i] = 0
+
+
+        print(games[i])
+    try:
+        print(id)
+    except:
+        pass
