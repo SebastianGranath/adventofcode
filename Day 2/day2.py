7b3ef4407ae25a7521e8a3777bf5d68866645aff
Sebastian Granath <sebgr736@student.liu.se>
2023-12-09 10:51:41 +0100

    STILL NOT WORKING FUCK YOU day 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -3,29 +3,31 @@ with open('day2input.txt', 'r') as f:
     data = f.readlines()
 
 answer = 0
-colors = [' red', ' green', ' blue']
+colors = ['red', 'green', 'blue']
 lims = [12, 13, 14]
 for line in data:
     line = line.split(':')
     id = line[0].split(' ')[-1]
     games = line[1].split(';')
-    results = np.zeros((3, len(games)))
+    results = np.zeros((len(games), 3))
 
     for i in range(len(games)):
         for j in range(len(colors)):
             try:
-                # ERRORS IN THIS SECTION
-                results[i][j] = games[i][games[i].index(colors[j])-1]
-                print(results[j][i])
+                # ERRORS IN THIS SECTION, missing out 20 -> only taking 0
+                results[i][j] = games[i][games[i].index(colors[j])-2]
+                temp = games[i].replace(',', '').split(' ')
+                for word in temp:
+                    temp2 = word.split(' ')
+                    print(temp2.index(colors[j]))
+                    print(word.index(colors[j]))
+                results[i][j] = temp[temp.index(colors[j]) - 1]
             except:
                 results[i][j] = 0
     print('Continue here')
 
-    if ( (results[0][:] <= lims[0]).all() and (results[1][:] <= lims[1]).all() and (results[2][:]<= lims[2]).all()):
+    if ( (results[:,0] <= lims[0]).all() and (results[:,1] <= lims[1]).all() and (results[:,2]<= lims[2]).all()):
         answer = answer + int(id)
 
+print(answer)
 
-    try:
-        print(answer)
-    except:
-        pass
