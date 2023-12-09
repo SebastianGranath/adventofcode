37dc6634db3f0edb4a9a5a2550fab4f974e0605f
Sebastian Granath <sebgr736@student.liu.se>
2023-12-09 12:41:10 +0100

    FINAL-FUCKING-FINALLY

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -2,10 +2,9 @@ import numpy as np
 with open('day2input.txt', 'r') as f:
     data = f.readlines()
 
-answer = 0
-colors = ['red', 'green', 'blue']
-lims = [12, 13, 14]
+answer = 0; colors = ['red', 'green', 'blue']; lims = [12, 13, 14]
 for line in data:
+    line = line.replace('\n','')
     line = line.split(':')
     id = line[0].split(' ')[-1]
     games = line[1].split(';')
@@ -14,20 +13,11 @@ for line in data:
     for i in range(len(games)):
         for j in range(len(colors)):
             try:
-                # ERRORS IN THIS SECTION, missing out 20 -> only taking 0
-                results[i][j] = games[i][games[i].index(colors[j])-2]
                 temp = games[i].replace(',', '').split(' ')
-                for word in temp:
-                    temp2 = word.split(' ')
-                    print(temp2.index(colors[j]))
-                    print(word.index(colors[j]))
-                results[i][j] = temp[temp.index(colors[j]) - 1]
+                results[i][j] = temp[temp.index(colors[j])-1]
             except:
                 results[i][j] = 0
-    print('Continue here')
-
     if ( (results[:,0] <= lims[0]).all() and (results[:,1] <= lims[1]).all() and (results[:,2]<= lims[2]).all()):
         answer = answer + int(id)
-
 print(answer)
 
